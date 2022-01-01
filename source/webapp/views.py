3224cf50ae8c00f0b2555b4b4813ee6251908540
mtashtanovski <mtashtanovski@gmail.com>
2022-01-01 17:44:17 +0600

    Added returning of 404 status for non exist article.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponseNotFound, Http404
 from django.urls import reverse
 
 from webapp.models import Article
@@ -25,7 +25,11 @@ def create_article_view(request):
 
 
 def article_view(request, pk):
-    # pk = request.GET.get('pk')
-    article = Article.objects.get(pk=pk)
+
+    # try:
+    #     article = Article.objects.get(pk=pk)
+    # except Article.DoesNotExist:
+    #     raise Http404
+    article = get_object_or_404(Article, pk=pk)
     context = {'article': article}
     return render(request, 'article_view.html', context)
